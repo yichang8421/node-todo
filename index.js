9682032d780bd84ca54246414b95a39171735a6e
andy8421 <yichang8421@qq.com>
2021-10-23 20:49:11 +0800

    重构代码，所有操作拆分成函数

--- index.js
+++ index.js
@@ -15,11 +15,84 @@ module.exports.clear = async () => {
     await db.write([]);
 }
 
-module.exports.showAll = async () => {
-    // 读取之前的任务
-    const list = await db.read();
-    // 使用 inquirer.js 勾销任务
+function markAsDone(list, Index) {
+    list[Index].done = true;
+    // 存入文件
+    db.write(list);
+}
 
+function markAsUnDone(list, Index) {
+    list[Index].done = false;
+    db.write(list);
+}
+
+function updateTitle(list, Index) {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'title',
+            message: "What's new title",
+            default: list[Index].title
+        })
+        .then((answer3) => {
+            list[Index].title = answer3.title;
+            db.write(list);
+        });
+}
+
+function deleteTask(list, Index) {
+    list.splice(Index, 1);
+    db.write(list);
+}
+
+function askForAction(list, Index) {
+
+    inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: "Which task to operate ",
+        choices: [
+            {name: "◀ come back", value: "quit"},
+            {name: "completed", value: "markAsDone"},
+            {name: "undone", value: "markAsUnDone"},
+            {name: "rename", value: "updateTitle"},
+            {name: "- deleteTask", value: "deleteTask"},
+        ]
+    }).then((answer2) => {
+        switch (answer2.action) {
+            case "markAsDone":
+                markAsDone(list, Index);
+                break;
+            case "markAsUnDone":
+                markAsUnDone(list, Index);
+                break;
+            case "updateTitle":
+                updateTitle(list, Index);
+                break;
+            case "deleteTask":
+                deleteTask(list, Index);
+                break;
+        }
+    });
+}
+
+function askForCreateTask(list) {
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'title',
+            message: "What's new task",
+        })
+        .then((answer4) => {
+            list.push({
+                title: answer4.title,
+                done: false
+            })
+            db.write(list);
+        });
+}
+
+function printTasks(list) {
     inquirer
         .prompt({
             type: 'list',
@@ -39,64 +112,19 @@ module.exports.showAll = async () => {
         .then((answer) => {
             const Index = Number(answer.index)
             if (Index >= 0) {
-                // 选中任务
-                inquirer.prompt({
-                    type: "list",
-                    name: "action",
-                    message: "Which task to operate ",
-                    choices: [
-                        {name: "◀ come back", value: "quit"},
-                        {name: "completed", value: "markAsDone"},
-                        {name: "undone", value: "markAsUnDone"},
-                        {name: "rename", value: "updateTitle"},
-                        {name: "- delete", value: "delete"},
-                    ]
-                }).then((answer2) => {
-                    switch (answer2.action) {
-                        case "markAsDone":
-                            list[Index].done = true;
-                            // 存入文件
-                            db.write(list);
-                            break;
-                        case "markAsUnDone":
-                            list[Index].done = false;
-                            db.write(list);
-                            break;
-                        case "updateTitle":
-                            inquirer
-                                .prompt({
-                                    type: 'input',
-                                    name: 'title',
-                                    message: "What's new title",
-                                    default: list[Index].title
-                                })
-                                .then((answer3) => {
-                                    list[Index].title = answer3.title;
-                                    db.write(list);
-                                });
-                            break;
-                        case "delete":
-                            list.splice(Index, 1);
-                            db.write(list);
-                            break;
-                    }
-                });
-                console.log(answer.index);
+                // 询问操作的任务
+                askForAction(list, Index);
             } else if (Index === -2) {
                 // 创建新任务
-                inquirer
-                    .prompt({
-                        type: 'input',
-                        name: 'title',
-                        message: "What's new task",
-                    })
-                    .then((answer4) => {
-                        list.push({
-                            title: answer4.title,
-                            done: false
-                        })
-                        db.write(list);
-                    });
+                askForCreateTask(list);
             }
         });
+}
+
+module.exports.showAll = async () => {
+    // 读取之前的任务
+    const list = await db.read();
+
+    // 使用 inquirer.js 勾销任务
+    printTasks(list);
 }
